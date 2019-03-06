2b3f7f221e573faf1a42b9ddb9ec5e330de06840
Rocío <37368725+rocioberzalcabetas@users.noreply.github.com>
2019-03-06 13:39:11 +0100

    SumaPosicionesParesVectores

---
+++ SumaPosicionesParesVector.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#define N 10
+
+int main()
+{
+	int v[N], v1[5], suma, i;
+	
+	for (i=0; i<N; i++)
+	{
+		printf("Introduzca los valores del vector\n");
+		scanf("%i", &v[i]);
+	}
+	
+	for (i=0; i<N; i++)
+	{
+		if (i%2==0)
+		{
+			printf("Vector posiciones pares: %i\n", v[i]);
+			suma+=v[i];
+		}
+	}
+	
+	printf("Suma: %i", suma);
+}
