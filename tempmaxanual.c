089f26249e230d8cae123ade9c1a24e03a5a1b99
Rosa1Silverio <36919748+Rosa1Silverio@users.noreply.github.com>
2019-03-06 12:59:59 +0100

    CAMBIOS PARA ENTENDER EL CODIGO MEJOR

--- tempmaxanual.c
+++ tempmaxanual.c
@@ -10,34 +10,40 @@ int main()
 {
 	
 	float tempanual[12],tempmaxima;
-	int mes[12]={1,2,3,4,5,6,7,8,9,10,11,12},mesfinal;
+	// Se podria hacer con :int mes[12]={1,2,3,4,5,6,7,8,9,10,11,12};
+	//Para almacenar el valor de los meses (ENERO,...)
+	int mesfinal;
 	int i,p;
 	
 	for(i=0; i<12; i++){
-	
-		printf("Introduce la temperatura en grados centigrados del mes %d\n",mes[i]);
+		
+		//En caso de que uses mes[12] se realiza:
+		//printf("Introduce la temperatura en grados centigrados del mes %d\n",mes[i]);
+		printf("Introduce la temperatura en grados centigrados del mes %d ",i+1);
 		scanf("%f", &tempanual[i]);
 		
 	}
 	
-	//Almacena la tempmax la 1º temp
+	//Almacena la tempmaxima como  la 1º temperatura
 	tempmaxima=tempanual[0];
 	//Almacena el mes de la temperatura maxima
-	mesfinal=1
+	mesfinal=1;
 	
 	for(p=1 ; p<12 ; p++)
 	{
 		if(tempanual[p]>tempmaxima)
 		{
-			tempmaxima=tempanua[p];
-			mesfinal=mes[p];
+			tempmaxima=tempanual[p];
+			// Si usas mes[12]:
+			//mesfinal=mes[p];
+			mesfinal=p+1;
 		}
 		
 	}
 	
 	
 	
-	printf("la temperatura maxima es %f grados centigrados del mes %d ",tempmax, mesfinal);
+	printf("la temperatura maxima es %f grados centigrados del mes %d ",tempmaxima, mesfinal);
 		
 return 0;	
 
