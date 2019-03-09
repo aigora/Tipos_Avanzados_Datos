7dacf93114d70839fec16d9a867a2f4bdb959cb0
Rocío <37368725+rocioberzalcabetas@users.noreply.github.com>
2019-03-09 18:03:09 +0100

    Ejercicio 8 vectores

---
+++ FuncionMatematica.c
@@ -0,0 +1,34 @@
+/*  ROCIO BERZAL CABETAS
+	Q203
+	TEMA 4 Ejercicio 8 vectores: realizar una funcion matematica
+*/
+
+#include <stdio.h>
+#include<math.h>
+
+int main()
+{
+	int i, n;
+	float x, r;
+	
+	printf("Elige el tamano del vector:");
+	scanf("%i", &n);
+	
+	int v[n];
+	
+	printf("Introduce el vector:");
+	for (i=0; i<n; i++)
+	{
+		scanf("%i", &v[i]);
+	}
+	
+	for (i=0; i<n; i++)
+	{
+		x+=pow (v[i], 2.0);
+		r=sqrt (x);
+	}
+	
+	printf("Resultado %f", r);
+	
+	
+}
