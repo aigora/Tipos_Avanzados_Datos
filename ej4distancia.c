f1272ca02d5c1e9c5b6984b788611e0963011882
Diego54632 <47477240+Diego54632@users.noreply.github.com>
2019-03-10 19:51:56 +0100

    Update ej4distancia.c
    Se corrige la repeticion. Tambien, es innecesario el uso de if y else ya que no importa que salgan numeros negativos, pues al cuadrado estos se convierten en positivos.

--- ej4distancia.c
+++ ej4distancia.c
@@ -1,55 +1,34 @@
 /*
-Autor: Manuel Dégano
+Autor: Manuel DÃ©gano
 Grupo: Q103
-Descripción: Este programa imprime la distancia entre dos puntos en el espacio
+DescripciÃ³n: Este programa imprime la distancia entre dos puntos en el espacio
 */
 
 #include <stdio.h>
+#include <math.h>
 
 void main() 
 {
-int  v1[2], v2[2], v3[2];
+float  v1[2], v2[2], v3[2];
 float dist;
+int i;
 	
-	printf("introduce las coordenadas del vector 1 y 2\n");
-	scanf("%i", &v1[0]);
-	scanf("%i", &v1[1]);
-	scanf("%i", &v2[0]);
-	scanf("%i", &v2[1]);
-	
-	if (v1[0]>v2[0] && v1[1]>v2[1])
-	{	
-	v3[0]=v1[0]-v2[0];
-	v3[1]=v1[1]-v2[1];
-	dist=sqrt((v3[0]*v3[0])+(v3[1]*v3[1]));
-	printf("la distancia es  %f\n", dist);
-	printf("el vector distancia es v3={ %i , %i }", v3[0], v3[1]);
+	for(i=0;i<2;i++)
+	{
+		printf("Introduzca el elemento %i del primer vector\n", i+1);
+		scanf("%f", &v1[i]);
+		printf("Introduzca el elemento %i del segundo vector\n", i+1);
+		scanf("%f", &v2[i]);
 	}
 	
-	else if (v1[0]<v2[0] && v1[1]>v2[1])
-	{
-	v3[0]=v2[0]-v1[0];
-	v3[1]=v1[1]-v2[1];
-	dist=sqrt((v3[0]*v3[0])+(v3[1]*v3[1]));
-	printf("la distancia es  %f\n", dist);
-	printf("el vector distancia es v3={ %i , %i }", v3[0], v3[1]);
-	}
 	
-	else if (v1[0]>v2[0] && v1[1]<v2[1])
-	{
-	v3[0]=v1[0]-v2[0];
-	v3[1]=v2[1]-v1[1];
-	dist=sqrt((v3[0]*v3[0])+(v3[1]*v3[1]));
-	printf("la distancia es  %f\n", dist);
-	printf("el vector distancia es v3={ %i , %i }", v3[0], v3[1]);
-	}
-	
-	else if (v1[0]<v2[0] && v1[1]<v2[1])
-	{
 	v3[0]=v2[0]-v1[0];
 	v3[1]=v2[1]-v1[1];
+
 	dist=sqrt((v3[0]*v3[0])+(v3[1]*v3[1]));
 	printf("la distancia es  %f\n", dist);
-	printf("el vector distancia es v3={ %i , %i }", v3[0], v3[1]);
-	}
+	
+	
+	
+
 }
