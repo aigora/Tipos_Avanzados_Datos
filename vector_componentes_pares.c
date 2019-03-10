e0837113baa9c000d4543cace8648fa7e7f5877e
Maricrussi <47519598+Maricrussi@users.noreply.github.com>
2019-03-10 17:21:42 +0100

    Vector de los componentes pares

---
+++ vector_componentes_pares.c
@@ -0,0 +1,21 @@
+/* Maria Cruz Batista Gil
+Grupo: Q103
+Descripción: Este programa imprime en un nuevo vector los números de los componentes pares.*/
+
+#include <stdio.h>
+#define elementos 10
+
+int main (){
+	
+	int i, a[elementos]={1,2,3,4,5,6,7,8,9,10}, vectorpares[elementos/2];
+	
+	for (i=0;i<10;i=i+2){ //i=i+2 el programa va de dos en dos.
+		
+		vectorpares[i]=a[i];
+		printf("%d",vectorpares[i]);
+	}
+	
+	
+	
+	return 0;
+}
