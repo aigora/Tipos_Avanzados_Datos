0f35d06829edb995e45aeee203bc8409162c56f9
Maricrussi <47519598+Maricrussi@users.noreply.github.com>
2019-03-10 22:07:18 +0100

    Update vector_componentes_pares.c

--- vector_componentes_pares.c
+++ vector_componentes_pares.c
@@ -1,20 +1,21 @@
 /* Maria Cruz Batista Gil
 Grupo: Q103
-Descripción: Este programa imprime en un nuevo vector los números de los componentes pares.*/
+DescripciÃ³n: Este programa imprime en un nuevo vector los nÃºmeros de los componentes pares.*/
 
 #include <stdio.h>
 #define elementos 10
 
 int main (){
 	
-	int i, a[elementos]={1,2,3,4,5,6,7,8,9,10}, vectorpares[elementos/2];
+	int i, a[elementos]={1,2,3,4,5,6,7,8,9,10}, vectorpares[elementos/2], suma=0;
 	
 	for (i=0;i<10;i=i+2){ //i=i+2 el programa va de dos en dos.
 		
 		vectorpares[i]=a[i];
-		printf("%d",vectorpares[i]);
+		printf("%d \n",vectorpares[i]);
+		suma+=vectorpares[i];
 	}
-	
+	printf("La suma de sus componentes es:%d",suma);
 	
 	
 	return 0;
