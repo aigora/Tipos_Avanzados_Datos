9b1efa811949337095767ab0a6d26fb232e25029
martaesparcialopez <37370553+martaesparcialopez@users.noreply.github.com>
2019-03-25 11:15:26 +0100

    Update Cadena de caracteres imprmir frase al reves.c

--- Cadena de caracteres imprmir frase al reves.c
+++ Cadena de caracteres imprmir frase al reves.c
@@ -1,25 +1,33 @@
 /*Marta Esparcia
 Grupo:Q103*/
-/*Ejercicio: Introduzca una frase cualquiera por teclado e imprímala al	revés.	(ejemplo: casa à asac).*/
+
+/*Ejercicio	2: Introduzca una	frase	cualquiera	por	teclado	e	imprímala	al	revés.	(ejemplo:	casa	à asac).*/
 #include<stdio.h>
 int main (){
 	char frase[50], letra;
-	int i,longitudfrase=0;
+	int i,j,longitudfrase=0, aux=0;
 	
 	printf("Introduce una frase\n");
 	gets(frase);
 	printf("La frase introducida es:%s\n",frase);
-	
-	i=0;
-	while(frase[i]!='0'){
+	i=0; 
+
+	while(frase[i]!='\0'){
 		longitudfrase++;
 		i++;
 		}
+	printf("Long es %i\n",longitudfrase);
+	
 	i=0;
-	for(i=longitudfrase-1;i>=0;i--){
+	j=0;
+	for(i=0;i<longitudfrase/2;i++){
+				aux=frase[i];
+				frase[i]=frase[longitudfrase-i-1];
+				frase[longitudfrase-i-1]=aux;
 		
-		printf("\n la frase es %c\t",frase[i]);
-	}
+		}
+		
+		printf("\n la frase es %s\t",frase);
 	
 	return 0;
 	}
