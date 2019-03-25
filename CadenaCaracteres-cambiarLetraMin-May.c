b5c44f4a173a3d2c971c2509044c5e2782418220
mariacanga <47222561+mariacanga@users.noreply.github.com>
2019-03-25 10:03:50 +0100

    Cambiar letra o texto a May o Min

--- CadenaCaracteres-cambiarLetraMin-May.c
+++ CadenaCaracteres-cambiarLetraMin-May.c
@@ -1,7 +1,7 @@
 /*
 AUTORA: Maria Canga
 GRUPO: Q-103
-DESCRIPCIÓN: Cambiar un texto o una letra especifica a mayusculas o minusculas.
+DESCRIPCIÃ“N: Cambiar un texto o una letra especifica a mayusculas o minusculas.
 */
 #include <stdio.h>
 
@@ -11,63 +11,68 @@ int main() {
 	
 	printf("Introducir texto:\n");
 	gets(texto);
-	
-	printf("1-Cambiar texto a mayusculas\n");
-	printf("2-Cambiar texto a minusculas\n");
-	printf("3-Cambiar letra a mayuscula\n");
-	printf("4-Cambiar letra a minuscula\n");
-	scanf("%d", &opcion);
-	
-	switch(opcion){
-		case 1:
-			i=0;
-			while(texto[i]!='\0') {
-				if(texto[i]>='a'&&texto[i]<='z'){
-					texto[i]-=32;
+	do{
+		printf("1-Cambiar texto a mayusculas\n");
+		printf("2-Cambiar texto a minusculas\n");
+		printf("3-Cambiar letra a mayuscula\n");
+		printf("4-Cambiar letra a minuscula\n");
+		printf("5-Fin del programa\n");
+		scanf("%d", &opcion);
+
+		switch(opcion){
+			case 1:
+				i=0;
+				while(texto[i]!='\0') {
+					if(texto[i]>='a'&&texto[i]<='z'){
+						texto[i]-=32;
+					}
+					i++;
 				}
-				i++;
-			}
-			printf("%s\n", texto);
-			break;
-		case 2:
-			j=0;
-			while(texto[j]!='\0') {
-				if(texto[j]>='A'&&texto[j]<='Z'){
-					texto[j]+=32;
+				printf("%s\n", texto);
+				break;
+			case 2:
+				j=0;
+				while(texto[j]!='\0') {
+					if(texto[j]>='A'&&texto[j]<='Z'){
+						texto[j]+=32;
+					}
+					j++;
 				}
-				j++;
-			}
-			printf("%s\n", texto);
-			break;
-		case 3:
-			printf("Introduce la letra:\n");
-			fflush(stdin);
-			scanf("%c", &letra);
-			
-			k=0;
-			while(texto[k]!='\0') {
-				if(texto[k]==letra){
-					texto[k]-=32;
+				printf("%s\n", texto);
+				break;
+			case 3:
+				printf("Introduce la letra:\n");
+				fflush(stdin);
+				scanf("%c", &letra);
+
+				k=0;
+				while(texto[k]!='\0') {
+					if(texto[k]==letra){
+						texto[k]-=32;
+					}
+					k++;
 				}
-				k++;
-			}
-			printf("%s\n", texto);
-			break;
-		case 4:
-			printf("Introduce la letra:\n");
-			fflush(stdin);
-			scanf("%c", &letra);
-			
-			l=0;
-			while(texto[l]!='\0') {
-				if(texto[l]==letra){
-					texto[l]+=32;
+				printf("%s\n", texto);
+				break;
+			case 4:
+				printf("Introduce la letra:\n");
+				fflush(stdin);
+				scanf("%c", &letra);
+
+				l=0;
+				while(texto[l]!='\0') {
+					if(texto[l]==letra){
+						texto[l]+=32;
+					}
+					l++;
 				}
-				l++;
-			}
-			printf("%s\n", texto);
-			break;
-		default:
-			printf("Opcion incorrecta");
+				printf("%s\n", texto);
+				break;
+			case 5:
+				printf("Fin del programa\n");
+				break;
+			default:
+				printf("Opcion incorrecta");
 		}
-}
+	}while(opcion!=5);
+
