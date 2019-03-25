651cfdc7e1036e3c59e92274b3769f8e2974b7f6
EstoKesss <47519473+EstoKesss@users.noreply.github.com>
2019-03-25 11:17:28 +0100

    Contador de una determinada letra en una cadena.

---
+++ ejerciciodecadenacontador.c
@@ -0,0 +1,22 @@
+//Carlota Stocks Galván, Q-103.
+//Contador de una determinada letra en una cadena.
+
+#include<stdio.h>
+int main()
+{
+char cadena[50];
+char letra;
+int i,cont=0;
+printf("Introduzca una letra:\n");
+scanf("%c",&letra);
+printf ("Escriba una palabra:\n");
+scanf("%s",cadena);
+i=0;
+while(cadena[i]!='\0'){
+if(cadena[i]==letra)
+cont++;
+i++;
+}
+printf("Numero de repeticiones de la letra %c : %d\n", letra,cont);
+
+}
