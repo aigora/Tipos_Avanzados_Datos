e1c0ac89442f386957c9b71d4ad83bc6ef8cfc90
lauraubeda <47366968+lauraubeda@users.noreply.github.com>
2019-03-25 10:31:50 +0100

    Lee una palabra y la escribe al revés

---
+++ Ejercicio cadena.c
@@ -0,0 +1,19 @@
+//Autora: Laura Úbeda Muñoz
+//Grupo: Q103
+//Descripción: Este programa lee una palabra y la escribe al revés
+
+#include <stdio.h>
+
+void main () {
+	char palabra [20];
+	int i=0;
+	
+	printf("Escriba una palabra:\n");
+	scanf("%s", palabra);
+
+while (palabra[i++]!='\0');
+printf("%s escrita al reves es:", palabra);
+
+while(i>1)
+printf("%c", palabra[--i]);
+}
