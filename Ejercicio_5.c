3558ff4a5ed178b54cff51c8d3af7814ceb8a34d
Diego54632 <47477240+Diego54632@users.noreply.github.com>
2019-04-10 19:46:49 +0200

    Ej5_estructuras

---
+++ Ejercicio_5.c
@@ -0,0 +1,97 @@
+/*
+Diego Gomez
+Q103
+Descripcion: pedir de nuevo el email si no lleva . o @
+*/
+#include<stdio.h>
+#include<string.h>
+
+struct contacto
+	{
+		char nombre[50];
+		int anio, mes;
+		char email[50];		
+		
+	}contacto1, contacto2;
+	
+int main()
+{
+	int i=0;
+	int validacion1, validacion1_2, validacion1_3;
+	int iguales;
+	
+	printf("Introduzca su nombre\n");
+	gets(contacto1.nombre);
+	printf("Introduzca el anio de nacimiento\n");
+	scanf("%i", &contacto1.anio);
+	printf("Introduzca el mes de nacimiento\n");
+	scanf("%i", &contacto1.mes);
+	do
+	{
+		printf("Introduzca el email\n");
+		fflush(stdin);
+		gets(contacto1.email);
+		
+		while(contacto1.email[i]!='\0')
+		{
+			if(contacto1.email[i]=='@')
+			{
+				validacion1=1;
+			}
+			
+			else if(contacto1.email[i]=='.')
+			{
+				validacion1_2=1;
+			}
+			i++;
+		}
+		
+		validacion1_3=validacion1+validacion1_2;
+	}while(validacion1_3!=2);
+	
+	
+	
+	printf("Introduzca su nombre\n");
+	gets(contacto2.nombre);
+	printf("Introduzca el anio de nacimiento\n");
+	scanf("%i", &contacto2.anio);
+	printf("Introduzca el mes de nacimiento\n");
+	scanf("%i", &contacto2.mes);
+	validacion1=0;
+	validacion1_2=0;
+	i=0;
+	do
+	{
+		printf("Introduzca el email\n");
+		fflush(stdin);
+		gets(contacto2.email);
+		
+		while(contacto2.email[i]!='\0')
+		{
+			if(contacto2.email[i]=='@')
+			{
+				validacion1=1;
+			}
+			else if(contacto2.email[i]=='.')
+			{
+				validacion1_2=1;
+			}
+			i++;
+		}
+		
+		validacion1_3=validacion1+validacion1_2;
+	}while(validacion1_3!=2);
+	
+	iguales=strcmp(contacto1.email, contacto2.email);//strcmp devuelve 0 si son iguales
+	if(iguales==0)
+	{
+		printf("Ambos usuarios tienen el mismo email\n");
+	}
+	else
+	{
+		printf("No coinciden los emails\n");
+	}
+}
+	
+	
+
